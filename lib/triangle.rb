361a3d2aced15b608df8d5e4e47938d59ff9330c
James Johnson <jtaylorjohnson@gmail.com>
2020-04-12 01:52:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,12 +9,13 @@ class Triangle
   end
   
   def kind
+    if :side1 = :side2 = :side3 
     :equilateral
-    If :side1 = :side2 = :side3 
+    If :side1 = :side2 or :side1 = :side3 or :side2 = :side3
     :isosceles
-    :side1 = :side2 or :side1 = :side3 or :side2 = :side3
+    If :side1 != :side2, :side1 != :side3, :side2 != :side3
     :scalene
-    :side1 != :side2, :side1 != :side3, :side2 != :side3
+    
   end
   
   valid? :side1 + :side2 > :side3, :side1 + :side3 > :side2, :side2 + :side3 > :side1 
