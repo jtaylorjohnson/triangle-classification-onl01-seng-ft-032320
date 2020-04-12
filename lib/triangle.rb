722426e609982b860fcb149623cb6d0aca90fa08
James Johnson <jtaylorjohnson@gmail.com>
2020-04-12 03:08:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -28,7 +28,7 @@ class Triangle
     @side1 = side1
     @side2 = side2
     @side3 = side3
-    if (@side1+@side2 > @side3) || (@side1+@side3 > @side2) || (@side2+@side3 > @side1) && @side1 >= 0 && @side2 >= 0 && @side3 >= 0
+    if (side1+ side2 > side3) || (side1 + side3 > side2) || (side2 + side3 > side1) && @side1 >= 0 && @side2 >= 0 && @side3 >= 0
     end
   end
   
