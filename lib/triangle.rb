eca4e665c0135f2d5970d5b210d954f9083e080b
James Johnson <jtaylorjohnson@gmail.com>
2020-04-12 02:07:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -15,13 +15,12 @@ class Triangle
       rescue TriangleError => error
           puts error.message
       end
-      elsif :side1 = :side2 = :side3 
+    elsif :side1 = :side2 = :side3 
       :equilateral
-      elsif :side1 = :side2 or :side1 = :side3 or :side2 = :side3
+    elsif :side1 = :side2 or :side1 = :side3 or :side2 = :side3
       :isosceles
-      elsif :side1 != :side2 && :side1 != :side3 && :side2 != :side3
+    elsif :side1 != :side2 && :side1 != :side3 && :side2 != :side3
       :scalene
-      end
     end
   end
   
