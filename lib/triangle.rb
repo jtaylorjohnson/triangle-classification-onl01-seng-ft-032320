85dfd85868da051bf5ba24bfb0587dd945c88456
James Johnson <jtaylorjohnson@gmail.com>
2020-04-12 02:23:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -3,29 +3,29 @@ class Triangle
   attr_accessor :side1, :side2, :side3, :equilateral, :isosceles, :scalene
   
   def initialize(side1, side2, side3)
-    side1 = :side1
-    side2 = :side2
-    side3 = :side3
+    side1 = @side1
+    side2 = @side2
+    side3 = @side3
   end
   
   def kind
-    if self.valid? != true 
+    if valid? != true 
       begin
         raise TriangleError
       rescue TriangleError => error
           puts error.message
       end
-    elsif :side1 == :side2 && :side2 == :side3 
+    elsif @side1 == @side2 && @side2 == @side3 
       :equilateral
-    elsif :side1 == :side2 or :side1 == :side3 or :side2 == :side3
+    elsif @side1 == @side2 or @side1 == @side3 or @side2 == @side3
       :isosceles
-    elsif :side1 != :side2 && :side1 != :side3 && :side2 != :side3
+    elsif @side1 != @side2 && @side1 != @side3 && @side2 != @side3
       :scalene
     end
   end
   
   def valid? 
-    if self.side1 + self.side2 > self.side3 or self.side1 + self.side3 > self.side2 or self.side2 + self.side3 > self.side1 && self.side1 != 0 && self.side2 != 0 && self.side3 != 0
+    if @side1 + @side2 > @side3 or @side1 + @side3 > @side2 or @side2 + @side3 > @side1 && @side1 != 0 && @side2 != 0 && @side3 != 0
     end
   end
   
