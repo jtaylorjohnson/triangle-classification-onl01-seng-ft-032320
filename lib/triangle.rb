99aa52591c86e209ef9d77253e334ab141ab5b6d
James Johnson <jtaylorjohnson@gmail.com>
2020-04-12 03:07:27 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -25,6 +25,9 @@ class Triangle
   end
   
   def valid?
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
     if (@side1+@side2 > @side3) || (@side1+@side3 > @side2) || (@side2+@side3 > @side1) && @side1 >= 0 && @side2 >= 0 && @side3 >= 0
     end
   end
